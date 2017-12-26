f028bb3b620e86b74b76948236dd6e05b359b617
Zwilla <michael.padilla@zwilla.de>
2017-12-27 00:59:05 +0100

    copyright update and new BCH logo

--- Views/backend/payment_bitcoincash/view/main/list.js
+++ Views/backend/payment_bitcoincash/view/main/list.js
@@ -1,6 +1,7 @@
 /*
  * (c) LX <lxhost.com@gmail.com>
- *
+ * (c) 2017 Miguel Padilla <miguel.padilla@zwilla.de>
+ * Donations: BCH:1L81xy6FoMHpNWxFtKTKGbsz9Sye1sSpSp BTC:1kD11aS83Du87EigaCodD8HVYmurHgT6i  ETH:0x8F2E4fd2f76235f38188C2077978F3a0B278a453
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
